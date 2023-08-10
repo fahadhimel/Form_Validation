ebda8a5db4c2a2ca47359dfc1c54fba71bcf9444
fahad himel <fahadhimel2@gmail.com>
2023-08-10 16:30:11 +0600

    Office_2

--- index.js
+++ index.js
@@ -6,65 +6,121 @@ choosefile.addEventListener("change", () => {
 });
 
 const firstname = document.getElementById("firstname");
-const lastname = document.getElementById("lastname");
-const fname = document.getElementById("fname");
-const mname = document.getElementById("mname");
-const pphone = document.getElementById("pphone");
-const department = document.getElementById("department");
-const email = document.getElementById("email");
-const phone = document.getElementById("phone");
-const blad = document.getElementById("blad");
-const country = document.getElementById("country");
-const address = document.getElementById("address");
-const regular = document.getElementById("regular");
-const date = document.getElementById("date");
-const errorp = document.getElementById("error");
-const form = document.getElementById("form");
+const pfirst = document.getElementById("pfirst");
 
-const lengthFunction = (idname) => {
+const lastname = document.getElementById("lastname");
+const plast = document.getElementById("plast");
+
+const fname = document.getElementById("fname");
+const pfname = document.getElementById("pfname");
+
+const mname = document.getElementById("mname");
+const pmname = document.getElementById("pmname");
+
+const pphone = document.getElementById("pphone");
+const ppphone = document.getElementById("ppphone");
+
+const department = document.getElementById("department");
+const pdepartment = document.getElementById("pdepartment");
+
+const email = document.getElementById("email");
+const pemail = document.getElementById("pemail");
+
+const phone = document.getElementById("phone");
+const psphone = document.getElementById("psphone");
+
+const blad = document.getElementById("blad");
+const pblad = document.getElementById("pblad");
+
+const country = document.getElementById("country");
+const pcountry = document.getElementById("pcountry");
+
+const address = document.getElementById("address");
+const paddress = document.getElementById("paddress");
+
+const radio = document.getElementsByClassName("radio");
+const pradio = document.getElementById("pradio");
+
+const regular = document.getElementById("regular");
+const day = document.getElementById("day");
+const pStudentType = document.getElementById("pregular");
+
+const date = document.getElementById("date");
+const pdate = document.getElementById("pdate");
+
+const errorp = document.getElementById("error");
+//const errorp = document.getElementById("error");
+
+const form = document.getElementById("form");
+//const form = document.getElementById("form");
+
+const lengthFunction = (idname, ptag) => {
+  ptag.innerText = "";
   idname.style.border = "";
   const { name, value } = idname;
   if (value.length < 4) {
-    errorp.innerText += `${name} length is to low. `;
+    ptag.innerText = `${name} length is to low. `;
     idname.style.border = "1px solid red";
+    return false;
   } else if (value.length > 50) {
-    errorp.innerText += `${name} length is to high. `;
+    ptag.innerText = `${name} length is to high. `;
     idname.style.border = "1px solid red";
+    return false;
   }
+  return true;
 };
 
-const emailValidation = (email) => {
+const emailValidation = (email, ptag) => {
+  ptag.innerText = "";
   email.style.border = "";
-  lengthFunction(email);
+  const returnnull = lengthFunction(email, ptag);
+  if (!(returnnull)) {
+    return false;
+  }
   const { name, value } = email;
   if (value.indexOf("@") <= 0) {
-    errorp.innerText += `${name} is in invalid. `;
+    ptag.innerText = `${name} is in invalid. `;
     email.style.border = "1px solid red";
+    return false;
   } else if (
     value.charAt(value.length - 4) != "." &&
     value.charAt(value.length - 3) != "."
   ) {
-    errorp.innerText += `${name} is invalid. `;
+    ptag.innerText = `${name} is invalid. `;
     email.style.border = "1px solid red";
+    return false;
+  }
+
+  return true;
+};
+
+const handleKeyPress = (e) => {
+  if (!/[0-9]/.test(e.key)) {
+    e.preventDefault();
   }
 };
 
-const phoneValidation = (phone) => {
+const phoneValidation = (phone, ptag) => {
+  ptag.innerText = "";
   phone.style.border = "";
   const { name, value } = phone;
   if (value.length < 11 || value.length > 16) {
-    errorp.innerText += `${name} is invalid. `;
+    ptag.innerText = `${name} is invalid. `;
     phone.style.border = "1px solid red";
+    return false;
   }
+  return true;
 };
 
-const deparBladFunc = (DeBl) => {
+const deparBladFunc = (DeBl, ptag) => {
+  ptag.innerText = "";
   DeBl.style.border = "";
   const { name, value } = DeBl;
   if (name === "department") {
     if (value !== "CSE" && value !== "EEE") {
-      errorp.innerText += `${name} is invalid. `;
+      ptag.innerText += `${name} is invalid. `;
       DeBl.style.border = "1px solid red";
+      return false;
     }
   } else if (name === "Blad Group") {
     if (
@@ -75,37 +131,112 @@ const deparBladFunc = (DeBl) => {
       value !== "AB+" &&
       value !== "AB-"
     ) {
-      errorp.innerText += `${name} is invalid. `;
+      ptag.innerText = `${name} is invalid. `;
       DeBl.style.border = "1px solid red";
+      return false;
     }
+  }
+  return true;
+};
+
+const dateValidation = (date, ptag) => {
+  ptag.innerText = "";
+  date.style.border = "";
+  const { name, value } = date;
+  const year = new Date().getFullYear().toString() - 17;
+
+  if (Number(value.slice(0, 4)) == 0) {
+    ptag.innerText = `${name} is Empty. `;
+    date.style.border = "1px solid red";
+    return false;
+  } else if (Number(value.slice(0, 4)) >= Number(year)) {
+    ptag.innerText = `${name} is invalid.minimum value 2005 `;
+    date.style.border = "1px solid red";
+    return false;
+  }
+  return true;
+};
+const radioValidation = (radio, ptag) => {
+  ptag.innerText = "";
+  document.getElementById("radioall").style.border = "";
+
+  if (radio[0].checked || radio[1].checked || radio[2].checked) {
+    return true;
+  } else {
+    ptag.innerText = "Gender not Checked. ";
+    document.getElementById("radioall").style.border = "1px solid red";
+    return false;
   }
 };
 
-const validation = () => {
-  lengthFunction(firstname);
-  lengthFunction(lastname);
-  lengthFunction(fname);
-  lengthFunction(mname);
+const studentTypeFunction = (regular, day, pStudentType) => {
 
-
-  phoneValidation(pphone);
-  phoneValidation(phone);
-
-  emailValidation(email);
-  
-  deparBladFunc(department);
-  deparBladFunc(blad);
-
-  
+  pStudentType.innerText = "";
+  document.getElementById("gendate").style.border = "";
+  if (!(regular.checked)) {
+    pStudentType.innerText = `Student Type ${regular.name} not Checked`;
+    document.getElementById("gendate").style.border = "1px solid red";
+    return false;
+  } else if (!(day.checked)) {
+    pStudentType.innerText = `Student Type ${day.name} not Checked`;
+    document.getElementById("gendate").style.border = "1px solid red";
+    return false;
+  }
+  return true;
 };
 
-const radiovalue = (value) => {
-  console.log(value);
+const validation = () => {
+  const c1 = lengthFunction(firstname, pfirst);
+  const c2 = lengthFunction(lastname, plast);
+  const c3 = lengthFunction(fname, pfname);
+  const c4 = lengthFunction(mname, pmname);
+  const c5 = phoneValidation(pphone, ppphone);
+  const c6 = phoneValidation(phone, psphone);
+  const c7 = emailValidation(email, pemail);
+  const c8 = deparBladFunc(department, pdepartment);
+  const c9 = deparBladFunc(blad, pblad);
+  const c10 = lengthFunction(country, pcountry);
+  const c11 = lengthFunction(address, paddress);
+  const c12 = dateValidation(date, pdate);
+  const c13 = radioValidation(radio, pradio);
+  const c14 = studentTypeFunction(regular, day, pStudentType);
+
+  console.log(Boolean(c10) )
+
+  if (
+    c1 &&
+    c2 &&
+    c3 &&
+    c4 &&
+    c5 &&
+    c6 &&
+    c7 &&
+    c8 &&
+    c9 &&
+    c10 &&
+    c11 &&
+    c12 &&
+    c13 &&
+    c14
+
+  ) {
+    return true;
+  }else{
+    return false;
+  }
+
+  
 };
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   errorp.innerHTML = "";
 
-  validation();
+  const submit = validation();
+  console.log(submit);
+
+  if (submit) {
+    alert("Form Submited Successfully");
+    console.log("click");
+  }
 });
